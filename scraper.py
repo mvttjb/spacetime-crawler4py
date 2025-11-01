4d5c4af8c0b67aa6f4ab9f8a090856911de283d5
Matthew Bautista <matthjb2@uci.edu>
2025-10-31 22:32:29 -0700

    Missed a non-html page

--- scraper.py
+++ scraper.py
@@ -103,7 +103,7 @@ def is_valid(url):
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|wav|avi|mov|mpeg|mpg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
@@ -178,4 +178,3 @@ def save_report(filename="crawler_report.json"):
     }
     with open(filename, "w") as f:
         json.dump(data, f, indent=2)
-    
