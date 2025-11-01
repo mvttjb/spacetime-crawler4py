4d5226dda02629604157b476077dcc8e0fb29e03
Matthew Bautista <matthjb2@uci.edu>
2025-10-31 21:44:37 -0700

    Removed single letters from being counted.

--- scraper.py
+++ scraper.py
@@ -152,9 +152,10 @@ def read_page(url, resp):
     # Ignore all stop words!
     STOP_WORDS = {
         "a","about","above","after","again","against","all","am","an","and","any", "are","as","at","be","because","been","before","being","below","between",
-        "both","but","by","could","d","did","do","does","doing","down","during","each", "few","for","from","further","had","has","have","having","he","her","here","hers","herself","him","himself","his","how","i","if","in","into","is", "it","its","itself","just","me","more","most","my","myself","no","nor", "not","now","of","off",
+        "both","but","by","could","d","did","do","does","doing","down","during","each", "few","for","from","further","had","has","have","having","he","her","here","hers","herself","him","himself","his","how","if","in","into","is", "it","its","itself","just","me","more","most","my","myself","no","nor", "not","now","of","off",
         "on","once","only","or","other","our","ours","ourselves","out","over","own","s","same","she","should","so","some","such", "than","that","the","their","theirs","them","themselves","then","there","these","they","this","those","through","to","too","under","until","up","very","was","we","were","what","when","where","which","while","who",
-        "whom","why","with","would","you","your","yours","yourself","yourselves"
+        "whom","why","with","would","you","your","yours","yourself","yourselves","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x",
+        "y","z"
     }
     global_word_counter.update(w for w in words if w not in STOP_WORDS)
 
